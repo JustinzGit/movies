db210b4a05eba6bc008a87b7c7aa6fe5f73927b2
Justin Branch <justinms@outlook.com>
2020-02-09 00:47:30 +0000

    movie controller progress

--- lib/movies.rb
+++ lib/movies.rb
@@ -1,8 +1,12 @@
 # Environment file, will require all the classes we build inside movies
 
-require "movies/version"
+require_relative "./movies/version"
+require_relative "./movies/movie_controller"
 
-module Movies
-  class Error < StandardError; end
-  # Your code goes here...
-end
+
+# Defining namespace, but not needed, version file takes care of this
+# Whats purpose of this namespace?
+# module Movies
+#   class Error < StandardError; end
+#   # Your code goes here...
+# end
---
+++ lib/movies/movie_controller.rb
@@ -0,0 +1,36 @@
+# My CLI controller
+
+class MovieController
+  
+  def call
+    puts "Welcome to Movie Finder!"
+    puts "The CLI that searches for movies playing in your area."
+    puts "Enter your zip code to list theaters: "
+    theater_list
+    movie_list
+  end
+  
+  def theater_list
+    puts "1. REGAL EDWARDS GREENWAY GRAND PALACE SCREENX & RPX -- 3839 Weslayan, Houston, TX 77027"
+    puts "2. AMC HOUSTON 8 -- 510 Texas Street, Houston, TX 77002"
+  end 
+  
+  def movie_list
+    puts "Select a movie theater to list movies currently playing."
+    puts "To re-enter a zip code type 'menu'"
+    puts "To exit Movie Finder type 'exit'"
+    
+    input = gets.strip
+    case input
+    when "1"
+      puts "movie info on theater 1"
+    when "2"
+      puts "movie info on theater 2"
+    when "menu"
+      call
+    else
+      puts "Invalid input!"
+      movie_list
+    end 
+  end 
+end 
