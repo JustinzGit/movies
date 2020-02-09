19c8c60c1d1dfcec6e327d86f66b339b6f9d9408
Justin Branch <justinms@outlook.com>
2020-02-09 01:27:39 +0000

    small theater class changes

--- lib/movies/theater.rb
+++ lib/movies/theater.rb
@@ -11,10 +11,19 @@ class Theater
   
   def theaters_by_zipcode
     # returns list of theaters via zipcode
+    
+    # scrape site based on zipcode and populate 
+    @name = name
+    @location = location
+    @@all << self
   end 
   
   def movies_by_theater
     # returns list of movies playing at that theater
+  end 
+  
+  def self.clear
+    # method to clear @@all if a new zipcode is inputted
   end 
   
 end 
