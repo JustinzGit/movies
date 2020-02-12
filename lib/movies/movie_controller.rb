a6e6f8c32c9b9a9cb56ef0ded502152cede6a6ae
Justin Branch <justinms@outlook.com>
2020-02-12 04:56:08 +0000

    factored methods in controller

--- lib/movies/movie_controller.rb
+++ lib/movies/movie_controller.rb
@@ -19,20 +19,14 @@ class MovieController
       Scrape.scrape_theaters(input)
       puts ""
       puts ""
-      Theater.all.each.with_index(1) do |theater, i|
-        puts "#{i}. #{theater.name} -- #{theater.distance}"
-        puts "    Location: #{theater.location}"
-        puts "    Phone: #{theater.phone_number}"
-        puts ""
-      end
+      show_theaters
     
     elsif input == "exit"
       exit_program
     
     else
-      puts "                ! ~ ! ~ ! ~ ! ~"
-      puts "                 Invalid Input!"
-      puts "                ! ~ ! ~ ! ~ ! ~"
+      puts ""
+      puts "          ! ~ ! ~ ! ~ ! ~ Invalid Input ~ ! ~ ! ~ ! ~ !"
       puts ""
       call
     end 
@@ -46,15 +40,10 @@ class MovieController
     print "Selection: "
     input = gets.strip.downcase
 
-    
     if input.to_i > 0
       puts ""
       Scrape.scrape_movies(Theater.all[input.to_i - 1].url)
-      Movie.all.each.with_index(1) do |movie, i|
-        puts "#{i}. #{movie.name}"
-        puts "    Show times: #{movie.show_times}"
-        puts ""
-      end
+      show_movies
       list_movie_info
       
     elsif input == 'menu'
@@ -66,9 +55,8 @@ class MovieController
       exit_program
     
     else
-      puts "                ! ~ ! ~ ! ~ ! ~"
-      puts "                 Invalid Input!"
-      puts "                ! ~ ! ~ ! ~ ! ~"
+      puts ""
+      puts "! ~ ! ~ ! ~ ! ~ Invalid Input ~ ! ~ ! ~ ! ~ !"
       puts ""
       list_movies
     end 
@@ -77,13 +65,55 @@ class MovieController
   def list_movie_info
     puts "~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~"
     puts "Enter the movie number for more information."
+    puts "To go back to theater list type 'theaters'"
     puts "To re-enter a zip code type 'menu'"
     puts "To exit Movie Finder type 'exit'"
     print "Selection: "
     input = gets.strip.downcase
     
     if input.to_i > 0
-       movie = Movie.all[input.to_i - 1]
+       show_movie_info(input)
+       list_movie_info
+       
+    elsif input == 'menu'
+      Theater.clear
+      Movie.clear
+      call
+      
+    elsif input == 'theaters'
+      show_theaters
+      list_movie_info
+
+    elsif input == 'exit'
+      exit_program
+  
+    else
+      puts ""
+      puts "! ~ ! ~ ! ~ ! ~ Invalid Input ~ ! ~ ! ~ ! ~ !"
+      puts ""
+      list_movie_info
+    end 
+  end
+  
+  def show_theaters
+   Theater.all.each.with_index(1) do |theater, i|
+      puts "#{i}. #{theater.name} -- #{theater.distance}"
+      puts "    Location: #{theater.location}"
+      puts "    Phone: #{theater.phone_number}"
+      puts ""
+    end
+  end
+  
+  def show_movies
+    Movie.all.each.with_index(1) do |movie, i|
+      puts "#{i}. #{movie.name}"
+      puts "    Show times: #{movie.show_times}"
+      puts ""
+    end
+  end
+  
+  def show_movie_info(input)
+    movie = Movie.all[input.to_i - 1]
        Scrape.scrape_movie_info(movie.url, movie)
        puts ""
        puts "~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~"
@@ -99,24 +129,7 @@ class MovieController
        puts ""
        puts "#{movie.distributors}"
        puts ""
-       list_movie_info
-       
-    elsif input == 'menu'
-      Theater.clear
-      Movie.clear
-      call
-      
-    elsif input == 'exit'
-      exit_program
-  
-    else
-      puts "                ! ~ ! ~ ! ~ ! ~"
-      puts "                 Invalid Input!"
-      puts "                ! ~ ! ~ ! ~ ! ~"
-      puts ""
-      list_movie_info
-    end 
-  end 
+     end 
   
   def exit_program
     puts ""
